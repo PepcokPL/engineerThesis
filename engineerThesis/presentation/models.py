79ca7b3b81d4af01fb4ef36cab0770981a4ec9ad
Pepcok <Pepcok@Peplap>
2012-07-17 01:42:08 +0200

    Added models to presentation

---
+++ engineerThesis/presentation/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+#todo dodaæ not nulle
+
+class Presentation(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+class Slide(models.Model):
+    content = models.TextField()
+    description = models.TextField(blank=True)
+    presentation = models.ForeignKey(Presentation)
+    id_title = models.CharField(max_length=20) #TODO walidacja o braku spacji itp
+    created_at = models.DateTimeField()
+    order_number = models.IntegerField()
+    
+    class Meta:
+        unique_together = (("presentation","order_number"),)
+
+class Position(models.Model):
+    id = models.ForeignKey(Slide, primary_key=True)
+    pos_x = models.IntegerField()
+    pos_y = models.IntegerField()
+    pos_z = models.IntegerField(blank=True)
+    rotation = models.IntegerField(blank=True)
