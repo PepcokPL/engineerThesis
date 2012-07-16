e229fda889e75260b8f7c90c4dd491a25d2da1b9
Pepcok <Pepcok@Peplap>
2012-07-17 01:41:36 +0200

    First Commit

--- engineerThesis/settings.py
+++ engineerThesis/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'C:\\Users\\Pepcok\\Workspaces\\PythonWorkspace\\engineerThesis\\sqlite.db',                      # Or path to database file if using sqlite3.
+        'NAME': 'engineerthesis',                      # Or path to database file if using sqlite3.
         'USER': 'engineer',                      # Not used with sqlite3.
         'PASSWORD': 'trustme',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -109,6 +109,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    'C:\\Users\\Pepcok\\Workspaces\\PythonWorkspace\\engineerThesis\\engineerThesis\\templates'
 )
 
 INSTALLED_APPS = (
@@ -119,9 +120,11 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'polls',
+    'presentation',
 )
 
 # A sample logging configuration. The only tangible logging
